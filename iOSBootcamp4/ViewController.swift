76f9a5742b73853d6e4d6624cec06b7674ba3b46
Muhammad Hanif <muhammad_hanif@telkomsel.co.id>
2022-12-07 06:26:20 +0700

    First Commit

---
+++ iOSBootcamp4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSBootcamp4
+//
+//  Created by Muhammad Hanif on 02/12/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
